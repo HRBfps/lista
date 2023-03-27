adba88f0c021f89ba6ff7d07cca23cf87811f7e1
HRB.7 <84103822+HRBfps@users.noreply.github.com>
2023-03-27 12:22:18 -0300

    eita

---
+++ lista/js/2.js
@@ -0,0 +1,3 @@
+let n = parseInt(prompt("Digite o numero da tabuada"))
+for(i = 1; i<=10; i++)
+    document.write('<section>'+n+' x '+i+' =', n*i +'</section>')
---
+++ lista/js/3.js
@@ -0,0 +1,4 @@
+let n = 0
+for(i=0;i<1000;i++)
+    n=i+i
+alert("O resultado de 1+2+3...1000: "+n)
